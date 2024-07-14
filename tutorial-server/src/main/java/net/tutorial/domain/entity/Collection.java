b3663cdf1325d7d89b2f684e78a6ae1ed5f82f3a
aigsy <68892478+aigsy@users.noreply.github.com>
2024-07-14 23:10:59 +0800

    点赞、收藏记录表关系实体

---
+++ tutorial-server/src/main/java/net/tutorial/domain/entity/Collection.java
@@ -0,0 +1,13 @@
+package net.tutorial.domain.entity;
+
+/**
+ * 用户收藏关系
+ * @author guansiyu
+ * @date 2024/7/14
+ */
+public class Collection {
+    private Long collectionId;
+    private Long userId;
+    private int type;
+    private Long objectId;
+}
---
+++ tutorial-server/src/main/java/net/tutorial/domain/entity/Like.java
@@ -0,0 +1,14 @@
+package net.tutorial.domain.entity;
+
+/**
+ * 点赞关系
+ * @author guansiyu
+ * @date 2024/7/14
+ */
+public class Like {
+    private Long likeId;
+    private Long userId;
+    private int type;
+    private Long objectId;
+
+}
